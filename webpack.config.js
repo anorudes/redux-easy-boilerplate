6f5991905745e50eca74801d972d7e7dfea41ddb
keske <hello@andreykeske.com>
2015-08-11 00:09:36 +0300

    Add import-loader to fix jqeury dev in bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,6 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var autoprefixer = require('autoprefixer-core');
 var csswring = require('csswring');
 
-// Custome Bootstrap config
-require('./src/scripts/bootstrap.config.js');
-
 module.exports = {
 
   entry: [
@@ -35,31 +32,31 @@ module.exports = {
 
   module: {
     loaders: [{
-        test: /bootstrap\/js\//,
-        loader: 'imports?jQuery=jquery'
-      }, {
-        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url?limit=10000&minetype=application/font-woff"
-      }, {
-        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url?limit=10000&minetype=application/font-woff2"
-      }, {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url?limit=10000&minetype=application/octet-stream"
-      }, {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "file"
-      }, {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "url?limit=10000&minetype=image/svg+xml"
-      }, {
-        test: /\.js$/,
-        loaders: ['react-hot', 'babel-loader?optional[]=runtime&stage=0&plugins=jsx-control-statements/babel'],
-        exclude: /node_modules/
-      }, {
-        test: /\.scss$/,
-        loader: "css!postcss-loader!sass"
-      }]
+      test: /bootstrap\/js\//,
+      loader: 'imports?jQuery=jquery'
+    }, {
+      test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000&minetype=application/font-woff"
+    }, {
+      test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000&minetype=application/font-woff2"
+    }, {
+      test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000&minetype=application/octet-stream"
+    }, {
+      test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "file"
+    }, {
+      test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+      loader: "url?limit=10000&minetype=image/svg+xml"
+    }, {
+      test: /\.js$/,
+      loaders: ['react-hot', 'babel-loader?optional[]=runtime&stage=0&plugins=jsx-control-statements/babel'],
+      exclude: /node_modules/
+    }, {
+      test: /\.scss$/,
+      loader: "css!postcss-loader!sass"
+    }]
   },
   postcss: function() {
     return [autoprefixer, csswring];
