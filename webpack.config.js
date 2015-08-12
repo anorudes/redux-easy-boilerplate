fa4d1c60806b7ac9ec96983220708f4845bfb914
anorudes <anorudes@gmail.com>
2015-08-12 13:00:14 +0500

    remove dir "scripts"

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:3000',
     'webpack/hot/only-dev-server',
-    './src/scripts/index'
+    './src/index'
   ],
 
   output: {
