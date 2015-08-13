8926b5cd2ac355c2397c8af6a87a569ee01e028c
keske <hello@andreykeske.com>
2015-08-13 12:00:39 +0300

    Remove notes from .js

--- generator/component/temp.js
+++ generator/component/temp.js
@@ -7,20 +7,6 @@ import styles from './<%= name %>.styles.js';
 // Actions
 import { example } from '../../actions';
 
-// To export component add line to the ./src/components/index.js:
-// export { default as <%= name %> } from './<%= name %>/<%= name %>.js';
-//
-// And to import component ./src/Main.js
-// import { <%= name %> } from './components/';
-//
-// Actions
-// Add line to the ./src/actions/index.js:
-// export { <%= name %> } from './<%= name %>.js';
-//
-// Reducers
-// Add line:
-// export { <%= name %> } from './<%= name %>.js';
-
 export default class <%= name %> extends Component {
   render() {
     const { dispatch, data } = this.props;
--- generator/simple-component/temp.js
+++ generator/simple-component/temp.js
@@ -4,14 +4,6 @@ import { connect } from 'react-redux';
 // Component styles
 import styles from './<%= name %>.styles.js';
 
-// Note:
-//
-// To export compontn add line to the ./src/components/index.js:
-// export { default as <%= name %> } from './<%= name %>/<%= name %>.js';
-//
-// And to import component ./src/Main.js
-// import { <%= name %> } from './components/';
-
 export default class <%= name %> extends Component {
   render() {
     return (
