308bea1c51b1ab52278dabf8341fe4d640dcecb3
anorudes <anorudes@gmail.com>
2015-08-18 22:05:30 +0500

    clean up

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ import styles from './styles.js';
 export default class Footer extends Component {
   render() {
     return (
-      <div className={`${ styles }`} >
+      <div className={`${ styles }`}>
         <a href="https://github.com/anorudes/redux-easy-boilerplate">GitHub</a>
       </div>
     );
