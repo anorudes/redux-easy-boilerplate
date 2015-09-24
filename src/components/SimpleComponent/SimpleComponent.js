1ff2012ece79f92b0c0bb06e6d81447185ddd6a1
keske <hello@andreykeske.com>
2015-09-24 13:56:44 +0300

    Add document meta data

--- src/components/SimpleComponent/SimpleComponent.js
+++ src/components/SimpleComponent/SimpleComponent.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import DocumentMeta from 'react-document-meta';
 
 // Component styles
 import styles from './SimpleComponent.styles.js';
 
 export default class SimpleComponent extends Component {
   render() {
+    const metaData = {
+      title: 'SimpleComponent Title',
+      description: 'I am a description, and I can create multiple tags',
+      canonical: 'http://example.com/path/to/page',
+      meta: {
+        charset: 'utf-8',
+        name: {
+          keywords: 'react,meta,document,html,tags',
+        },
+      },
+    };
     return (
       <div className={ styles }>
+        <DocumentMeta {...metaData} />
         <h2>
           SimpleComponent
         </h2>
