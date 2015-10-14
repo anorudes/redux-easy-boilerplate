ffa54508a7cb060cfb96d0901be6e15db902bb68
anorudes <anorudes@gmail.com>
2015-10-14 21:24:31 +0500

    fix component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
 export { default as Simple } from './Simple';
 export { default as Items } from './Items';
-export { Header } from './Header';
+export { default as Header } from './Header';
 export { default as Footer } from './Footer';
