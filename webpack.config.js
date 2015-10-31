f84abc618846779e479ae851042e4217a9b9dfec
anorudes <anorudes@gmail.com>
2015-10-31 16:12:00 +0500

    minetype => mimetype #10

--- webpack.config.js
+++ webpack.config.js
@@ -38,19 +38,19 @@ module.exports = {
       loader: 'imports?jQuery=jquery'
     }, {
       test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-      loader: "url?limit=10000&minetype=application/font-woff"
+      loader: "url?limit=10000&mimetype=application/font-woff"
     }, {
       test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-      loader: "url?limit=10000&minetype=application/font-woff2"
+      loader: "url?limit=10000&mimetype=application/font-woff2"
     }, {
       test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-      loader: "url?limit=10000&minetype=application/octet-stream"
+      loader: "url?limit=10000&mimetype=application/octet-stream"
     }, {
       test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
       loader: "file"
     }, {
       test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-      loader: "url?limit=10000&minetype=image/svg+xml"
+      loader: "url?limit=10000&mimetype=image/svg+xml"
     }, {
       test: /\.js$/,
       loaders: ['react-hot', 'babel?stage=0&loose[]=es6.modules'],
