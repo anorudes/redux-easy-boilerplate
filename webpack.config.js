9d842370b55ca451a5bf48e6fa7fda523acf5ca1
keske <hello@andreykeske.com>
2015-11-05 13:27:38 +0300

    Add support images like *.jpg/png, also add support *.otf fonts

--- webpack.config.js
+++ webpack.config.js
@@ -8,13 +8,13 @@ module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
     'webpack-hot-middleware/client',
-    './src/index'
+    './src/index',
   ],
 
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, '/dist/'),
-    publicPath: '/dist/'
+    publicPath: '/dist/',
   },
 
   plugins: [
@@ -23,44 +23,53 @@ module.exports = {
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin(),
     new webpack.ProvidePlugin({
-      jQuery: "jquery"
-    })
+      jQuery: 'jquery',
+    }),
   ],
 
   resolve: {
     extensions: ['', '.jsx', '.js', '.json'],
-    modulesDirectories: ['node_modules', 'src']
+    modulesDirectories: ['node_modules', 'src'],
   },
 
   module: {
     loaders: [{
       test: /bootstrap\/js\//,
-      loader: 'imports?jQuery=jquery'
+      loader: 'imports?jQuery=jquery',
     }, {
       test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
-      loader: "url?limit=10000&mimetype=application/font-woff"
+      loader: 'url?limit=10000&mimetype=application/font-woff',
     }, {
       test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-      loader: "url?limit=10000&mimetype=application/font-woff2"
+      loader: 'url?limit=10000&mimetype=application/font-woff2',
     }, {
       test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-      loader: "url?limit=10000&mimetype=application/octet-stream"
+      loader: 'url?limit=10000&mimetype=application/octet-stream',
+    }, {
+      test: /\.otf(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url?limit=10000&mimetype=application/font-otf',
     }, {
       test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-      loader: "file"
+      loader: 'file',
     }, {
       test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-      loader: "url?limit=10000&mimetype=image/svg+xml"
+      loader: 'url?limit=10000&mimetype=image/svg+xml',
+    }, {
+      test: /\.png(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url?limit=10000&mimetype=image/png',
+    }, {
+      test: /\.jpg(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'url?limit=10000&mimetype=image/jpg',
     }, {
       test: /\.js$/,
       loaders: ['react-hot', 'babel?stage=0&loose[]=es6.modules'],
-      exclude: /node_modules/
+      exclude: /node_modules/,
     }, {
       test: /\.scss$/,
-      loader: "css!postcss-loader!sass"
-    }]
+      loader: 'css!postcss-loader!sass',
+    }],
   },
   postcss: function() {
     return [autoprefixer, csswring];
-  }
+  },
 };
