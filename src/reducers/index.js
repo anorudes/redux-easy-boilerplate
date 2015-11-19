6873027f23d6b3bca2769576f482cf6aeff94d1f
keske <hello@andreykeske.com>
2015-11-19 14:12:59 +0300

    Spaces %)

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import { routeReducer } from 'redux-simple-router';
-import {reducer as formReducer} from 'redux-form';
+import { reducer as formReducer } from 'redux-form';
 import { items } from './items';
 
 const rootReducer = combineReducers({
