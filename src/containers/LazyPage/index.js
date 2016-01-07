168bdd6f38d5c3f866f79303b4d88ae8eda71597
Lihan Li <lilihan.it@gmail.com>
2016-01-07 23:16:27 +1100

    Added  css to lazy page

--- src/containers/LazyPage/index.js
+++ src/containers/LazyPage/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import './styles/lazy.scss';
+
 
 class LazyPage extends Component {
   render() {
     return (
       <section>
         <h1>Lazy page</h1>
-        <p>The contents of this page should be lazily loaded</p>
+        <p className={'lazy-class'}>The contents of this page should be lazily loaded</p>
       </section>
     );
   }
