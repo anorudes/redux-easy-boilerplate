a740f4785831ba7e3812724ef3373ff08741878e
Lihan Li <lilihan.it@gmail.com>
2016-01-07 22:32:03 +1100

    Rewrote the route in object format in order to get dynamic route working.

--- src/index.js
+++ src/index.js
@@ -5,19 +5,34 @@ import { Router, Redirect, useRouterHistory } from 'react-router';
 import { createHashHistory } from 'history'
 import { syncReduxAndRouter } from 'redux-simple-router';
 import configureStore from './store/configureStore';
-import routes from './routes';
+
+
+import { App } from 'containers/App';
+import { Home } from 'containers/Home';
+import { List } from 'containers/List';
 
 const history = useRouterHistory(createHashHistory)({ queryKey: false })
 const store = configureStore();
 
 syncReduxAndRouter(history, store);
 
-ReactDOM.render(
-  <Provider store={store}>
-    <Router history={history}>
-      <Redirect from="/" to="home" />
-      {routes}
-    </Router>
-  </Provider>,
-  document.getElementById('root')
+
+const rootRoute = {
+  component: 'div',
+  childRoutes: [{
+    path: '/',
+    component: require('./components/App'),
+    childRoutes: [
+      { path: 'home', component: Home },
+      { path: 'list', component: List }
+    ]
+  }]
+};
+
+
+render(
+    <Provider store={store}>
+        <Router history={history} routes={rootRoute}></Router>
+    </Provider>,
+    document.getElementById('root')
 );
