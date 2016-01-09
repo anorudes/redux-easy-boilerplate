24547670094beca176dc05f3022d98bd531f5ec3
Ollipekka Jalonen <ollipekka@mediabox.fi>
2016-01-09 17:09:06 +0200

    Fixed reducer hotloading. Babel 6 no longer tries to make ES6 default exports the result of module.exports

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -21,7 +21,7 @@ export default function configureStore(initialState) {
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
     module.hot.accept('../reducers', () => {
-      const nextRootReducer = require('../reducers/index');
+      const nextRootReducer = require('../reducers/index').default;
       store.replaceReducer(nextRootReducer);
     });
   }
