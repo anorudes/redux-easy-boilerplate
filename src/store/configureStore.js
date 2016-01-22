9dabd63fc837ada72550ac49931a4bd8d98f64ea
Araphel <bartekus@gmail.com>
2016-01-22 01:39:23 -0700

    All modules updated to most recent versions, history module removed since it's now part of react-router as browserHistory, few minor adjustments to configureStore to make it all work

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,26 +1,26 @@
-import { createStore, applyMiddleware } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import createLogger from 'redux-logger';
+import { syncHistory } from 'redux-simple-router';
 import rootReducer from '../reducers';
 
-const logger = createLogger({
-  collapsed: true,
-  predicate: () =>
+export default function configureStore(initialState, browserHistory) {
+  const routerMiddleware = syncHistory(browserHistory);
+
+  const logger = createLogger({
+    collapsed: true,
+    predicate: () =>
     process.env.NODE_ENV === `development`, // eslint-disable-line no-unused-vars
-});
+  });
 
-const createStoreWithMiddleware = applyMiddleware(
-  thunkMiddleware,
-  logger
-)(createStore);
+  let middleware = applyMiddleware(thunkMiddleware, logger, routerMiddleware);
 
-export default function configureStore(initialState) {
-  const store = createStoreWithMiddleware(rootReducer, initialState);
+  const store = middleware(createStore)(rootReducer, initialState);
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
     module.hot.accept('../reducers', () => {
-      const nextRootReducer = require('../reducers/index').default;
+      const nextRootReducer = require('../reducers').default;
       store.replaceReducer(nextRootReducer);
     });
   }
