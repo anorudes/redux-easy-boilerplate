1558c1c4764070c5f6b021e8517d097b57edc3bb
anorudes <anorudes@gmail.com>
2016-02-22 23:07:37 +0500

    revert history to hashHistory

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, browserHistory } from 'react-router';
+import { Router, useRouterHistory } from 'react-router';
+import { createHashHistory } from 'history';
 import configureStore from './store/configureStore';
 import routes from './routes';
 
-const store = configureStore({}, browserHistory);
+const history = useRouterHistory(createHashHistory)({ queryKey: false });
+const store = configureStore({}, history);
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={browserHistory} routes={routes}>
-    </Router>
+    <Router history={history} routes={routes} />
   </Provider>,
   document.getElementById('root')
 );
