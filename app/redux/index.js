e0d435f18d167f7e70a8191e4fa5b11003f10a68
anorudes <anorudes@gmail.com>
2016-04-30 15:49:30 +0500

    clean

--- app/redux/index.js
+++ app/redux/index.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
 
 import app from './modules/app/app';
 import posts from './modules/posts/posts';
 
 const rootReducer = combineReducers({
-  form: formReducer,
   app,
   posts,
 });
